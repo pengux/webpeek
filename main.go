d635f5f02b2f3ed6014883fc8a39680c7a30748b
Peter Nguyen <peter@mictis.com>
2020-04-12 08:29:41 +0200

    Refactor UI code into a separate function

--- main.go
+++ main.go
@@ -6,11 +6,8 @@ import (
 	"log"
 	"net/url"
 	"os"
-	"strings"
 	"sync"
 
-	"github.com/gdamore/tcell"
-	"github.com/rivo/tview"
 	"github.com/urfave/cli/v2"
 )
 
@@ -49,102 +46,7 @@ func run(c *cli.Context) error {
 
 	wg.Wait()
 
-	var out []string
-
-	uiTextView := tview.NewTextView()
-	uiTextView.
-		SetDynamicColors(true).
-		SetWrap(false).
-		SetBorder(false)
-
-	// Returns a new primitive which puts the provided primitive in the center and
-	// sets its size to the given width and height.
-	modal := func(p tview.Primitive, width, height int) tview.Primitive {
-		return tview.NewFlex().
-			AddItem(nil, 0, 1, false).
-			AddItem(tview.NewFlex().SetDirection(tview.FlexRow).
-				AddItem(nil, 0, 1, false).
-				AddItem(p, height, 1, false).
-				AddItem(nil, 0, 1, false), width, 1, false).
-			AddItem(nil, 0, 1, false)
-	}
-
-	screen, err := tcell.NewScreen()
-	if err != nil {
-		return err
-	}
-
-	err = screen.Init()
-	if err != nil {
-		return err
-	}
-
-	screenWidth, screenHeight := screen.Size()
-	width := screenWidth / 3 * 2
-	if width < 40 {
-		width = 40
-	}
-
-	height := screenHeight / 3 * 2
-	if height < 20 {
-		height = 20
-	}
-
-	pages := tview.NewPages().
-		AddPage("modal", modal(uiTextView, width, height), true, true)
-	uiApp := tview.NewApplication().SetRoot(pages, true).
-		SetScreen(screen).
-		SetFocus(uiTextView)
-
-	showPeek := func(p *peekedContent) {
-		content := []string{p.url.String()}
-
-		if p.err != nil {
-			content = append(content, "[red]"+p.err.Error()+"[white]")
-		}
-		content = append(content, p.h1s...)
-		content = append(content, p.markdown)
-
-		// uiTextView.SetTitle(p.title)
-		uiTextView.SetText(strings.Join(content, "\n\n")).
-			ScrollToBeginning()
-	}
-
-	uiTextView.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-		switch event.Rune() {
-		case 'l':
-			out = append(out, peeks.Value().url.String())
-
-			if !peeks.Next() {
-				uiApp.Stop()
-				return event
-			}
-		case 'q':
-			uiApp.Stop()
-			return event
-		case 'r':
-			peeks.Value().Reload()
-		case 'h':
-			fallthrough
-		case ' ':
-			if !peeks.Next() {
-				uiApp.Stop()
-				return event
-			}
-		}
-
-		showPeek(peeks.Value())
-		return event
-	})
-
-	showPeek(peeks.Value())
-	err = uiApp.Run()
-	if err != nil {
-		return err
-	}
-
-	fmt.Println(strings.Join(out, "\n"))
-	return nil
+	return draw(peeks)
 }
 
 // getInputURLs checks whether input are from arguments or Stdin and try to
